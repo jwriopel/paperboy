9e2703e7663ef3baf0c5de08b74d125d4c67d169
Joe <joe.riopel@gmail.com>
2017-08-30 11:12:17 -0400

    Don't make users wait for timer on start.

--- cmd/pbtail/main.go
+++ cmd/pbtail/main.go
@@ -25,21 +25,27 @@ func main() {
 	seenItems := make(map[string]paperboy.Item)
 	ichan := make(chan paperboy.Item, 100)
 
+	getItems := func() {
+		for item := range paperboy.GetAll(sources) {
+			if _, seen := seenItems[item.Url]; !seen {
+				ichan <- item
+				seenItems[item.Url] = item
+			}
+		}
+
+	}
+
 	go func() {
 		pollTimer := time.Tick(time.Duration(120) * time.Second)
 		for {
 			select {
 			case <-pollTimer:
-				for item := range paperboy.GetAll(sources) {
-					if _, seen := seenItems[item.Url]; !seen {
-						ichan <- item
-						seenItems[item.Url] = item
-					}
-				}
+				getItems()
 			}
 		}
 	}()
 
+	getItems()
 	for {
 		for item := range ichan {
 			log.Printf("%s - [%s] %s\n", item.Title, item.SourceName, item.Url)
